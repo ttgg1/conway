c09bb4173a839788f4bf72808c17d7ac79207aa4
Paul Geerken <paul.geerken@gmx.de>
2023-02-16 21:33:52 +0100

    fixed game logic

--- src/main.c
+++ src/main.c
@@ -7,10 +7,10 @@
 #include <SDL2/SDL.h>
 
 
-#define CELLS_SIZE 20
+#define CELLS_SIZE 100
 
 // Grid init
-const int grid_cell_size = 10;
+const int grid_cell_size = 5;
 const int grid_width = CELLS_SIZE;
 const int grid_height = CELLS_SIZE;
 
@@ -22,12 +22,7 @@ const int window_height = (grid_height * grid_cell_size) + 1;
 SDL_Color back_color = {22, 22, 22, 255};  // Barely Black
 SDL_Color line_color = {44, 44, 44, 255};  // Dark grey
 SDL_Color alive_color = {255, 255, 255, 255};  // White
-
-// Dark theme.
-SDL_Color grid_background = {22, 22, 22, 255};  // Barely Black
 SDL_Color grid_line_color = {44, 44, 44, 255};  // Dark grey
-SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
-SDL_Color grid_cursor_color = {255, 255, 255, 255};  // White
 
 // SDL Variables
 bool running = true;
@@ -37,64 +32,87 @@ SDL_Renderer *renderer;
 // Game Variables
 bool simulate = false;
 
+const unsigned int arr_size = sizeof(bool)*CELLS_SIZE*CELLS_SIZE;
+
 //Helpers
-bool isAlive(bool (*cells)[CELLS_SIZE*CELLS_SIZE], int index){
-  return (*cells)[index]; 
+bool isAlive(bool *cells, int index){
+  if(index >= 0 && index < CELLS_SIZE*CELLS_SIZE)
+    return cells[index];
+  printf("ARGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n");
+  return 0;
 }
-void flipCell(bool (*cells)[CELLS_SIZE*CELLS_SIZE], int index){
-  (*cells)[index] = !(*cells)[index];
+void flipCell(bool *cells,int index){
+  if(index >= 0 && index < CELLS_SIZE*CELLS_SIZE)
+    cells[index] = !cells[index];
+}
+int modulo(int a, int b) {
+  if (a >= 0) {
+    return a % b;
+  }
+  return b+(a % b);
 }
 
 // Game functions
-void tick(bool (*cells)[CELLS_SIZE*CELLS_SIZE], bool (*cells_next)[CELLS_SIZE*CELLS_SIZE]) {
-  
-  memcpy((*cells_next), (*cells), CELLS_SIZE*CELLS_SIZE * sizeof(bool));
-  for (int i = 0; i < CELLS_SIZE*CELLS_SIZE-1; i++) {
+void tick(bool *cells, bool *cells_next) { 
+  memcpy(cells_next, cells, arr_size);
+  for (int i = 0; i < CELLS_SIZE*CELLS_SIZE; i++) {
     int neigbours = 0;
     //calculate neigbours
-    if((*cells)[(i-grid_width-1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; //top left
-    if((*cells)[(i-grid_width+1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; //top right
+    if(cells[modulo((i-grid_width-1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; //top left
+    if(cells[modulo((i-grid_width+1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; //top right
 
-    if((*cells)[(i-grid_width)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // above
-    if((*cells)[(i+grid_width)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // below
+    if(cells[modulo((i-grid_width),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // above
+    if(cells[modulo((i+grid_width),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // below
   
-    if((*cells)[(i-1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // left
-    if((*cells)[(i+1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // right
+    if(cells[modulo((i-1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // left
+    if(cells[modulo((i+1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // right
 
-    if((*cells)[(i+grid_width-1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // bottom left
-    if((*cells)[(i+grid_width+1)%CELLS_SIZE*CELLS_SIZE] == 1)neigbours++; // bottom right
+    if(cells[modulo((i+grid_width-1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // bottom left
+    if(cells[modulo((i+grid_width+1),CELLS_SIZE*CELLS_SIZE)] == 1)neigbours++; // bottom right
   
+    /*
+    printf("Zelle %i\n",i);
+    printf("Nachbarn: %i\n",neigbours);
+    printf("Top Left %i \n",modulo((i-grid_width-1),CELLS_SIZE*CELLS_SIZE));
+    printf("Top Right %i \n",modulo((i-grid_width+1),CELLS_SIZE*CELLS_SIZE));
+    printf("AMOGUS %i \n",modulo((i-grid_width),CELLS_SIZE*CELLS_SIZE));
+    printf("below %i \n",modulo((i+grid_width),CELLS_SIZE*CELLS_SIZE));
+    printf("left %i \n",modulo((i-1),CELLS_SIZE*CELLS_SIZE));
+    printf("right %i \n",modulo((i+1),CELLS_SIZE*CELLS_SIZE));
+    printf("bottom left %i \n",modulo((i+grid_width-1),CELLS_SIZE*CELLS_SIZE));
+    printf("bottom right %i \n",modulo((i+grid_width+1),CELLS_SIZE*CELLS_SIZE));
+    printf("\n\n");
+    */
     //calculate if alive
-    if((*cells)[i] == 1){
+    if(isAlive(cells,i)){
       if(neigbours > 3 || neigbours < 2) {
-        (*cells_next)[i] = 0;
+        cells_next[i] = 0;
       }
-      else if(neigbours == 3){
-        (*cells_next)[i] = 1;
-      }
-      
-    }
+    } else if(neigbours == 3){
+        cells_next[i] = 1;
+    } 
   }
-  memcpy((*cells), (*cells_next), CELLS_SIZE*CELLS_SIZE * sizeof(bool));
+  // bufferswap
+  memcpy(cells, cells_next, arr_size);
 }
 
-void draw(bool (*cells)[CELLS_SIZE*CELLS_SIZE]) {  
+void draw(bool *cells) {  
   // Draw game
   SDL_SetRenderDrawColor(renderer, back_color.r,back_color.g,back_color.b,back_color.a);
   SDL_RenderClear(renderer);
   // Rect for cell drawing
   SDL_Rect cell = {0,0,grid_cell_size,grid_cell_size};
 
-  for(int i=0;i<CELLS_SIZE*CELLS_SIZE-1;i++) {
+  for(int i=0;i<CELLS_SIZE*CELLS_SIZE;i++) {
     if(!isAlive(cells,i)){
       // Move rect
       cell.x = (int)(i%grid_width)*grid_cell_size;
       cell.y = (int)floor(i/grid_width)*grid_cell_size;
       //change color
-      SDL_SetRenderDrawColor(renderer, back_color.r,back_color.g,back_color.b,back_color.a);
+      SDL_SetRenderDrawColor(renderer, grid_line_color.r,grid_line_color.g,grid_line_color.b,grid_line_color.a);
       //draw rect
       SDL_RenderDrawRect(renderer, &cell);
-    } else if (isAlive(cells,i)) {
+    } else {
       // Move rect
       cell.x = (int)(i%grid_width)*grid_cell_size;
       cell.y = (int)floor(i/grid_width)*grid_cell_size;
@@ -108,14 +126,17 @@ void draw(bool (*cells)[CELLS_SIZE*CELLS_SIZE]) {
 }
 
 int main(int argc, char *argv[]) {
+  double frameStart = 0;
+  double dtime = 0;
+  float fps = 0;
   //create dynamic 2d Array.
-  bool (*g_cells)[CELLS_SIZE*CELLS_SIZE] = malloc(sizeof *g_cells);
-  bool (*g_cells_next)[CELLS_SIZE*CELLS_SIZE] = malloc(sizeof *g_cells_next);
+  bool *g_cells = malloc(arr_size);
+  bool *g_cells_next = malloc(arr_size);
 
   for (int i = 0; i < CELLS_SIZE*CELLS_SIZE-1; i++) {
       // Acess 2D-Array using pointer iteration
-      (*g_cells)[i] = 0;
-      (*g_cells_next)[i] = 0;
+      g_cells[i] = 0;
+      g_cells_next[i] = 0;
   }
 
   // init SDL
@@ -146,12 +167,12 @@ int main(int argc, char *argv[]) {
             case SDLK_SPACE:
               tick(g_cells, g_cells_next);
               draw(g_cells);
+              break;
           }
 
         case SDL_MOUSEBUTTONDOWN:
           int index = (int) e.motion.y / grid_cell_size * grid_width + e.motion.x / grid_cell_size;
-          (*g_cells)[index] = !(*g_cells)[index];
-          draw(g_cells);
+          flipCell(g_cells, index);
           break;
         case SDL_QUIT:
           running = false;
@@ -161,10 +182,9 @@ int main(int argc, char *argv[]) {
     }
     if(simulate) {
       tick(g_cells, g_cells_next);
-      
     }
     draw(g_cells);
-  }
+    }
 
   free(g_cells);
   free(g_cells_next);
